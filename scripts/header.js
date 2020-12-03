d3035747033179d524da3721c12516a725530462
Kira Tanaka <kiratanaka0521@gmail.com>
2020-12-03 19:22:51 +0900

    header.js completed

---
+++ scripts/header.js
@@ -0,0 +1,23 @@
+const target = document.querySelector('.nav-trigger');
+const header = document.querySelector('.page-header');
+
+
+const cb = (entries) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            header.classList.remove('triggered');
+        } else {
+            header.classList.add('triggered');
+        }
+    });
+}
+
+const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0
+}
+
+const io = new IntersectionObserver(cb, options, {once: false});
+io.observe(target);
+
